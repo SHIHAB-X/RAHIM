d7e7d27d9295722172c9b4c64089464a5def4c7b
SHIHAB <142003220+SHIHAB-X@users.noreply.github.com>
2023-11-26 14:02:50 +0600

    Update RAHIM.py

--- RAHIM.py
+++ RAHIM.py
@@ -1,34 +1,3 @@
 import RAHIM
-import os, platform
- 
-try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from RAHIM import RAHIM
- 
-        main_menu()
- 
- 
- 
-elif bit == "32bit":
- 
-        from RAHIM import RAHIM
- 
- 
-        main_menu()
- 
  
  
