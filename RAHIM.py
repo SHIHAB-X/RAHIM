1ab040689b68e6ce564930503edbf673e25569ea
SHIHAB <142003220+SHIHAB-X@users.noreply.github.com>
2023-11-26 13:36:48 +0600

    Create RAHIM.py

---
+++ RAHIM.py
@@ -0,0 +1,34 @@
+import RAHIM
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from Extract import Extract
+ 
+        main_menu()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from Extract import Extract
+ 
+ 
+        main_menu()
+ 
+ 
+ 
